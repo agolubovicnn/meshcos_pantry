795ce9db574d159f0e644f34e0e76303c1029bf8
Aleksandar GoluboviĆ <agolubovicnn@gmail.com>
2019-03-07 13:11:15 +0000

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source_greeting")
     end
   end
+
+  def copyright_generator
+    MeshcoViewTool::Render.copyright 'Alexandar Golubovic', 'All rights reserved'
+  end
 end
