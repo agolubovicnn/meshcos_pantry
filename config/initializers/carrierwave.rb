c595510d033ed5a16cdb9cdb672dc2d528c619d1
Aleksandar GoluboviĆ <agolubovicnn@gmail.com>
2019-06-20 15:46:43 +0200

    Integrated cocoon for portfolio items nested form components

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
   config.aws_acl    = 'private'
-
+  
   config.asset_host = 'http://example.com'
 
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
