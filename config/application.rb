6692a35374ed0cfcc32292f37f53b7ce41b24b20
Aleksandar GoluboviĆ <agolubovicnn@gmail.com>
2019-10-25 15:58:50 +0200

    Updated Gemfile

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,9 @@ Bundler.require(*Rails.groups)
 module MeshcosPantry
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 5.2
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
